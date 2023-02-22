14e25a9ffb413fe85ed52b1c92647c413322ae34
minhh <minh.bui@blackbook.ai>
2023-02-22 15:13:10 +0700

    create repository pattern for Product model

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                obj.Title   = objFromDb.Title;
+                obj.ISBN = objFromDb.ISBN;
+                obj.Author = objFromDb.Author;
+                obj.Description = objFromDb.Description;
+                obj.ListPrice= objFromDb.ListPrice;
+                obj.Price = objFromDb.Price;
+                obj.Price50 = objFromDb.Price50;
+                obj.Price100 = objFromDb.Price100;
+
+            }
+        }
+    }
+}
