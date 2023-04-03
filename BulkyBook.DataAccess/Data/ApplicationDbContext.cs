cb9bbdd7305f0934e8ccf6835c593f4dd93bc90a
minhh <minh.bui@blackbook.ai>
2023-04-03 20:43:31 +0700

    Summary UI and model for orders

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,9 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<Company> Companies { get; set; }
     public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetail { get; set; }
+
 
 
 
